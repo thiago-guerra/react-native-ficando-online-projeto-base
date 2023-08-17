f5cbfa2150a4c4343ac30c17ba92d12d3a716fb3
thiago-guerra <thiago.cesa@hotmail.com>
2023-08-17 01:28:05 -0300

    criando repositório

--- src/Servicos/Requisicoes/Repositorios.js
+++ src/Servicos/Requisicoes/Repositorios.js
@@ -19,3 +19,13 @@ export async function EditarRepositorio(id, nome, data, idUsuario) {
         return 'erro';
     }
 }
+
+export async function CriarRepo(idUsuario, nome, data) {
+    try {
+        await api.post(`repos`, { name: nome, data: data, postId: idUsuario });
+        return 'sucesso';
+    } catch (error) {
+        console.log(error);
+        return 'erro';
+    }
+}
--- src/paginas/CriarRepositorio/index.js
+++ src/paginas/CriarRepositorio/index.js
@@ -1,10 +1,23 @@
 import React, { useState } from 'react';
 import { Text, View, TouchableOpacity, TextInput, Alert } from 'react-native';
 import estilos from './estilos';
-
+import { CriarRepo } from '../../Servicos/Requisicoes/Repositorios';
 export default function CriarRepositorio({ route, navigation }) {
     const [nome, setNome] = useState('');
     const [data, setData] = useState('');
+    const idUsuario = route.params.postId;
+
+    async function criar() {
+        const result = await CriarRepo(idUsuario, nome, data);
+        if (result == 'sucesso') {
+            alert('Repositório criado com sucesso!'); 
+            Alert.alert('Repositório criado com sucesso!');
+            navigation.goBack();  
+        }else   {
+            alert('Erro ao criar repositório!');
+            Alert.alert('Erro ao criar repositório!');
+        }
+    }
 
     return (
         <View style={estilos.container}>
@@ -12,13 +25,19 @@ export default function CriarRepositorio({ route, navigation }) {
                 placeholder="Nome do repositório"
                 autoCapitalize="none"
                 style={estilos.entrada}
+                value={nome}
+                onChangeText={setNome}
             />
             <TextInput
                 placeholder="Data de criação"
                 autoCapitalize="none"
                 style={estilos.entrada}
+                value={data}
+                onChangeText={setData}
             />
-            <TouchableOpacity style={estilos.botao}>
+            <TouchableOpacity style={estilos.botao}
+                onPress={criar}
+            >
                 <Text style={estilos.textoBotao}>
                     Criar
                 </Text>
